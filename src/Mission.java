d8f7b6810f7f89564f2ec3a484e1b447560a85ed
minhabean <smh000124@gmail.com>
2023-02-22 21:52:36 +0900

    6회차 미션

--- src/Mission.java
+++ src/Mission.java
@@ -1,32 +1,28 @@
-import java.util.ArrayList;
-import java.util.Arrays;
-import java.util.List;
 import java.util.Scanner;
 
 public class Mission {
 
     public static void main(String[] args) {
-        Library library = new Library(Arrays.asList(
-            new Book("클린코드(Clean Code)"),
-            new Book("객체지향의 사실과 오해"),
-            new Book("테스트 주도 개발(TDD)")
-        ));
-        List<Book> BookList = library.getBookList();
+        Scanner scanner = new Scanner(System.in);
         while (true) {
-            System.out.println("대여할 책의 번호를 입력하세요.");
-            for (int i = 0; i < BookList.size(); i++) {
-                Book book = BookList.get(i);
-                System.out.println(i + 1 + ". " + book.getName() + " - " + book.getStatus());
+            System.out.println("휴대폰 번호를 입력해주세요. ");
+            String input = scanner.nextLine();
+            PhoneNumber phoneNumber = new PhoneNumber(input);
+            String phoneNumber1 = PhoneNumber.getPhoneNumberFormally();
+            try {
+                if (phoneNumber1.startsWith("010") && phoneNumber1.length() == 11) {
+                    System.out.println("휴대폰 번호를 정상적으로 입력하셨습니다. 입력하신 휴대폰 번호는 "
+                        + phoneNumber1 + "입니다.");
+                } else {
+                    throw new IllegalArgumentException();
+                }
+            } catch (IllegalArgumentException e) {
+                if (phoneNumber1.length() != 11) {
+                    System.out.println("휴대폰 번호는 11글자여야 합니다.");
+                } else {
+                    System.out.println("휴대폰 번호는 010으로 시작해야 합니다.");
+                }
             }
-            Scanner sc = new Scanner(System.in);
-            int bookNum = sc.nextInt() - 1;
-
-            if (BookList.get(bookNum).isBorrowed()) {
-                System.out.println("대여 중인 책은 대여할 수 없습니다.");
-            } else {
-                System.out.println("정상적으로 대여가 완료되었습니다.");
-            }
-            BookList.get(bookNum).Borrow();
         }
     }
 }
